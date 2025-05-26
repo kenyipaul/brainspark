5a4cb80a26531cc0b0ab4807beeafa00cd240781
Kenyi Paul <kenyipaul69@gmail.com>
2025-05-26 08:02:38 +0200

    Add Roboto.ttf font and update app, main.scss, and about view, -Implemented responsive design for home and courses page

--- client/src/views/about.tsx
+++ client/src/views/about.tsx
@@ -5,9 +5,8 @@ export default function About() {
                 <h1>ABOUT US</h1>
             </div>
             <div className="content">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, temporibus nobis. Consequatur nesciunt exercitationem suscipit.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, asperiores, maxime illo animi commodi officiis atque optio et dolorum doloremque minus quibusdam recusandae! Deleniti quasi cumque autem corporis? Architecto voluptatibus possimus nihil perspiciatis officia reiciendis!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis unde praesentium pariatur, minima nesciunt est officiis impedit aperiam doloremque similique libero culpa error voluptatem ex assumenda quis voluptas explicabo iusto architecto dolores saepe? Earum dolores veniam, accusamus voluptates reiciendis harum accusantium. Et aspernatur porro ea amet ratione repellat quos sequi!</p>
+                <p>Welcome to BrainSpark! We believe that everyone deserves the opportunity to learn, grow, and achieve their full potential. In today's fast-paced world, continuous learning isn't just an advantage—it's a necessity. That's why we've built a platform dedicated to making high-quality education accessible to anyone, anywhere.</p>
+                <p>At BrainSpark, you'll find a diverse range of courses taught by expert instructors, designed to equip you with practical skills and knowledge that will make a real impact on your career and life. Whether you're looking to master a new technology, explore a creative passion, or advance in your current field, we're here to support your learning journey every step of the way. Join our vibrant community of learners and discover what you can achieve!</p>
             </div>
         </div>
     )
